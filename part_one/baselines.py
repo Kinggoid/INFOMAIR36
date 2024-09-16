c8ac5b67d58153075e0d697c9d3fc774cb8d51b2
OliverPopovic <30406037+OliverPopovic@users.noreply.github.com>
2024-09-16 17:46:06 +0200

    changed structure

---
+++ part_one/baselines.py
@@ -0,0 +1,55 @@
+# model that assigns labels to data based on the majority class of the training data
+class MajorityClassModel:
+    # initialize model with training labels
+    def __init__(self, labels):
+        # set majority class as the label that has the highest occurence in the list
+        self.majorityClass = max(labels,key=labels.count)
+
+    # assigns majority class label to input data
+    def test(self, input):
+        return self.majorityClass
+    
+    # evaluates model based on lists of test data and labels 
+    def evaluate(self, data, labels):
+        if len(data) != len(labels):
+            raise Exception("data and label lists are not the same size")
+        
+        # calculate accuracy of model
+        correct = 0
+        for i in range(0, len(data)):
+            if self.test(data[i]) == labels[i]:
+                correct += 1
+    
+        # return accuracy
+        return correct / len(data)
+        
+    def evaluate_ours(self, data, labels):
+        
+        training_accuracy = sum(self.labels == self.majorityClass) / len(self.labels)
+
+        return training_accuracy
+
+# model that assigns labels to data based on keyword matching rules
+class KeywordMatchingModel(MajorityClassModel):
+    def __init__(self, labels):
+        super().__init__(labels)
+
+    def test(self, input):
+        if "thank" in input:
+            return "thankyou"
+        elif "bye" in input:
+            return "bye"
+        elif "hi " in input or "hello" in input or "helo " in input:
+            return "hello"
+        elif "what" in input or "phone" in input or "address" in input:
+            return "request"
+        elif "yes" in input:
+            return "affirm"
+        elif "no" in input:
+            return "negate"
+        elif "looking" in input or "area" in input:
+            return "inform"
+        elif "else" in input:
+            return "reqalts"
+        else: 
+            return self.majorityClass
--- part_one/main.py
+++ part_one/main.py
@@ -1,7 +1,43 @@
 from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
 from baselines import MajorityClassModel, KeywordMatchingModel
-from nn_classifier import NNClassifier
-from logregression import LogisticRegressionModel
-from decisiontree import DecisionTreeModel
-from dialogue_system import DialogueSystem
 import pickle
+
+# DATASET CLEANING -----------------------------------------------------------
+
+# file_path = 'D:\School\MAIR\dialog_acts.dat'
+file_path = 'part_one\dialog_acts.dat'
+df = pd.read_csv(file_path, delimiter= '\t')
+df.columns = ['dialog_act']
+
+#cleaning dataset
+df_clean = df.drop_duplicates()
+
+#adding structure to database, separating dialog_acts and utterances
+df['utterance'] = df['dialog_act'].str.split().str[1:]
+df['dialog_act'] = df['dialog_act'].str.split().str[0]
+
+df_clean['utterance'] = df_clean['dialog_act'].str.split().str[1:]
+df_clean['dialog_act'] = df_clean['dialog_act'].str.split().str[0]
+
+#splitting data into train and test data
+utterances = df['utterance']
+labels = df['dialog_act']
+utterance_train, utterance_test, label_train, label_test = train_test_split(utterances, labels, test_size = 0.15)
+
+#doing the same for the clean dataset
+utterance_clean = df_clean['utterance']
+label_clean = df_clean['dialog_act']
+utterance_clean_train, utterance_clean_test, label_clean_train, label_clean_test = train_test_split(utterance_clean, label_clean, test_size = 0.15)
+
+
+mc_model =  MajorityClassModel(utterance_clean_train)
+# km_model =  KeywordMatchingModel(train_labels)
+
+# get accuracies
+mc_acc = mc_model.evaluate(utterance_clean_test, label_clean_test)
+print("Majority class model accuracy is", mc_acc)
+
+# km_acc = km_model.evaluate(test_insts, test_labels)
+# print("Keyword matching model accuracy is", km_acc)
