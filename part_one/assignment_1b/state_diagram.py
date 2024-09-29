81a687d298a402e194506a11ca02834fce90d367
Thijme Wortel <60653407+Kinggoid@users.noreply.github.com>
2024-09-29 11:51:26 +0200

    update

--- part_one/assignment_1b/state_diagram.py
+++ part_one/assignment_1b/state_diagram.py
@@ -3,11 +3,15 @@ from functions import *
 
 
 class State_diagram:
-    def __init__(self, states):
+    def __init__(self):
+        self.state = "welcome"
         self.area = None
-        self.food = None
-        self.price = None
-        self.states = states
+        self.food_type = None
+        self.price_range = None
+        self.response = True
+        self.restaurant = None
+        self.other_options = None
+
     
     def add_state(self, state):
         self.states.append(state)
@@ -51,3 +55,75 @@ class State_diagram:
                 if preference_dict[2] == "empty":
                     current_state == ask_price_state
                     return current_state
+                
+    
+    def next_state(self, user_input):
+        if self.state == "welcome":
+            if user_input == "inform" or user_input == "null":
+                self.state = "ask_area"
+                print("System: In what area would you like to eat?")
+                self.extract_preferences(user_input)
+                self.response = False
+                self.state = "ask_area" if not self.preferences["area"] else "ask_food_type"
+            else:
+                print("System: Hello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?")
+        
+        
+        
+        elif self.state == "ask_area":
+            self.area = user_input
+            self.state = "ask_food"
+            print("System: What type of food are you looking for?")
+        elif self.state == "ask_food":
+            self.food_type = user_input
+            self.state = "ask_price"
+            print("System: What type of price range are you looking for?")
+        elif self.state == "ask_price":
+            self.price_range = user_input
+            self.state = "double_check"
+            print(f"System: So you want to eat at {self.area} place?")
+        elif self.state == "double_check":
+            if user_input == "yes":
+                self.state = "suggest_restaurant"
+                print(f"System: {self.restaurant} is a nice restaurant to eat at")
+            else:
+                self.state = "no_match"
+                print("System: Sorry, such a restaurant does not exist")
+        elif self.state == "suggest_restaurant":
+            self.state = "give_info"
+            print(f"System: The info for this restaurant is {self.other_options}")
+        elif self.state == "give_info":
+            self.state = "endstate"
+            print("System: The conversation has ended.")
+        elif self.state == "no_match":
+            self.state = "endstate"
+            print("System: The conversation has ended.")
+        else:
+            self.state = "endstate"
+            print("System: The conversation has ended.")
+    
+
+    def next_state(self, user_input):
+
+        self.handle_state(dialog_act, user_input)
+
+
+    def run(self, model, vectorizer):
+        if self.state == "welcome":
+            print("hi")
+            return None
+        print("System: Hello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?")
+        while self.state != "endstate":
+            user_input = input("You: ").lower()
+            vectorized_user_input = vectorizer.transform([user_input])
+            #classifying user input using ML model
+            dialog_act = model.predict(vectorized_user_input)
+            self.next_state(user_input)
+
+
+            # if self.response:
+                
+            # else:
+            #     user_input = ""
+            #     self.response = True
+            #     self.next_state(user_input)
